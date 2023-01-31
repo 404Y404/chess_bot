c70722dd558671a58f4c465fe0b7875f9591b4b3
404Y404 <yura.4ernenko@gmail.com>
2023-01-31 19:56:54 +0000

    add board class and parse method

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class Board():
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get("https://www.chess.com/")
+
+
+    def parse(self):
+        old_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "div .piece")))
+        while True:
+            new_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "div .piece")))
+            for i in range(len(old_figures)):
+                if old_figures[i] != new_figures[i]:
+                    return old_figures[i], new_figures[i]
+
+
+def get_class(element):
+    return element.get_attribute("class")
+
+
 def main():
-    pass
+    board = Board()
+    input("Зайдите в игру и нажмите Enter")
+    while True:
+        hod = board.parse()
+        print(hod)
+
 
 if __name__ == '__main__':
     main()
