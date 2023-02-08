0533c0be6816a1e9845e1042576fdca0c378a1d8
404Y404 <yura.4ernenko@gmail.com>
2023-02-08 16:49:18 +0200

    add new parser

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ class Board():
         self.browser.get("https://www.chess.com/")
 
 
-    def parse(self):
-        old_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "#board-vs-personalities > .piece")))
-        while True:
-            print("start parse")
-            new_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "#board-vs-personalities > .piece")))
-            for i in range(len(old_figures)):
-                if old_figures[i] != new_figures[i] and "dragging" not in new_figures[i]:
-                    return old_figures[i], new_figures[i]
+    def parse(self, old_hod):
+        end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[contains(@class, 'highlight')][1]")
+        start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[contains(@class, 'highlight')][2]")
+        hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
+        while hod == old_hod or hod[2:] + hod[:2] == old_hod:
+            end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[2]")
+            start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[3]")
+            hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
+            if old_hod != hod and hod[2:] + hod[:2] != old_hod:
+                sleep(0.5)
+                end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[2]")
+                start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[3]")
+                hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
+        return hod
 
 
-    def move(self, element, x, y):
+    def move(self, element, x, y, is_end, figura):
         piece = self.browser.find_element(By.CSS_SELECTOR, element)
         action = webdriver.common.action_chains.ActionChains(self.browser)
         action.move_to_element_with_offset(piece, 5, 5)
@@ -34,6 +40,8 @@ class Board():
         action.move_to_element_with_offset(hint, 5, 5)
         action.click()
         action.perform()
+        if is_end:
+            self.browser.find_element(By.CSS_SELECTOR, f'class="promotion-piece w{figura}"')
 
 
 def get_class(element):
@@ -41,22 +49,30 @@ def get_class(element):
 
 
 def to_chess_com(hod):
+    if len(hod) > 4:
+        hod2 = hod
+        letters = "abcdefgh"
+        x = letters.index(hod2[2]) + 1
+        y = hod2[3]
+        hod2 = hod2[:2]
+        hod2 = hod2.replace(hod2[0], str(letters.index(hod2[0]) + 1), 1)
+        hod2 = f".square-{hod2}"
+        return hod2, x, y, True, hod[-1]
+    hod2 = hod
     letters = "abcdefgh"
-    x = letters.index(hod[2]) + 1
-    y = hod[3]
-    hod = hod[:2]
-    hod = hod.replace(hod[0], str(letters.index(hod[0]) + 1), 1)
-    hod = f".square-{hod}"
-    return hod, x, y
+    x = letters.index(hod2[2]) + 1
+    y = hod2[3]
+    hod2 = hod2[:2]
+    hod2 = hod2.replace(hod2[0], str(letters.index(hod2[0]) + 1), 1)
+    hod2 = f".square-{hod2}"
+    return hod2, x, y, False, ""
 
 
 def to_bot(hod):
     letters = "abcdefgh"
-    h1 = hod[0][16:]
-    h2 = hod[1][16:]
-    h1 = h1.replace(h1[0], letters[int(h1[0]) - 1], 1)
-    h2 = h2.replace(h2[0], letters[int(h2[0]) - 1], 1)
-    return h1 + h2
+    hod = hod[17:]
+    hod = hod.replace(hod[0], letters[int(hod[0]) - 1], 1)
+    return hod
 
 def main():
     board = chess.Board()
@@ -69,8 +85,16 @@ def main():
     while True:
         if c == 1:
             print("black")
-            hod = board_player.parse()
-            board.push(to_bot(hod))
+            hod = board_player.parse(hod)
+            move = chess.Move.from_uci(hod)
+            if move in board.legal_moves:
+                print(hod)
+                board.push(move)
+            else:
+                hod = hod[2:] + hod[:2]
+                print(hod)
+                move = chess.Move.from_uci(hod)
+                board.push(move)
         else:
             print("white")
             hod = str(engine.play(board, limit).move)
@@ -78,11 +102,8 @@ def main():
             board.push(move)
             print(hod)
             board_player.move(*to_chess_com(hod))
-        hod = board_player.parse()
         c += 1
         c %= 2
-
-
     sleep(100)
 
 
