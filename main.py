2327408364f77bc24a58a3eb9e6a13486ca0478f
404Y404 <yura.4ernenko@gmail.com>
2023-02-08 15:15:37 +0200

    fix move method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import chess
+import chess.engine
 
 class Board():
     def __init__(self):
@@ -9,29 +11,78 @@ class Board():
 
 
     def parse(self):
-        old_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "div .piece")))
+        old_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "#board-vs-personalities > .piece")))
         while True:
-            new_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "div .piece")))
+            print("start parse")
+            new_figures = list(map(get_class, self.browser.find_elements(By.CSS_SELECTOR, "#board-vs-personalities > .piece")))
             for i in range(len(old_figures)):
-                if old_figures[i] != new_figures[i]:
+                if old_figures[i] != new_figures[i] and "dragging" not in new_figures[i]:
                     return old_figures[i], new_figures[i]
 
 
     def move(self, element, x, y):
-        self.browser.find_element(By.CSS_SELECTOR, f"[class='{element}']").click()
+        piece = self.browser.find_element(By.CSS_SELECTOR, element)
+        action = webdriver.common.action_chains.ActionChains(self.browser)
+        action.move_to_element_with_offset(piece, 5, 5)
+        action.click()
+        action.perform()
         print("click")
-        sleep(2)
-        location = self.browser.find_element(By.CSS_SELECTOR, f"[class='hint square-{x}{y}']").location
+        print(element, x, y)
+        sleep(0.5)
+        hint = self.browser.find_element(By.CSS_SELECTOR, f".square-{x}{y}")
+        action = webdriver.common.action_chains.ActionChains(self.browser)
+        action.move_to_element_with_offset(hint, 5, 5)
+        action.click()
+        action.perform()
 
 
 def get_class(element):
     return element.get_attribute("class")
 
 
+def to_chess_com(hod):
+    letters = "abcdefgh"
+    x = letters.index(hod[2]) + 1
+    y = hod[3]
+    hod = hod[:2]
+    hod = hod.replace(hod[0], str(letters.index(hod[0]) + 1), 1)
+    hod = f".square-{hod}"
+    return hod, x, y
+
+
+def to_bot(hod):
+    letters = "abcdefgh"
+    h1 = hod[0][16:]
+    h2 = hod[1][16:]
+    h1 = h1.replace(h1[0], letters[int(h1[0]) - 1], 1)
+    h2 = h2.replace(h2[0], letters[int(h2[0]) - 1], 1)
+    return h1 + h2
+
 def main():
-    board = Board()
+    board = chess.Board()
+    board_player = Board()
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+    limit = chess.engine.Limit(time=1.0)
+
     input("Зайдите в игру и нажмите Enter")
-    board.move("piece bp square-87", 8, 6)
+    c = 0
+    while True:
+        if c == 1:
+            print("black")
+            hod = board_player.parse()
+            board.push(to_bot(hod))
+        else:
+            print("white")
+            hod = str(engine.play(board, limit).move)
+            move = chess.Move.from_uci(hod)
+            board.push(move)
+            print(hod)
+            board_player.move(*to_chess_com(hod))
+        hod = board_player.parse()
+        c += 1
+        c %= 2
+
+
     sleep(100)
 
 
