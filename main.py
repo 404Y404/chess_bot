58744110732f6f270d5974e72d93543e98f75a78
404Y404 <yura.4ernenko@gmail.com>
2023-02-17 15:38:43 +0200

    stable play v1

--- main.py
+++ main.py
@@ -11,37 +11,37 @@ class Board():
 
 
     def parse(self, old_hod):
-        end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[contains(@class, 'highlight')][1]")
-        start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[contains(@class, 'highlight')][2]")
+        end = self.browser.find_element(By.XPATH,
+                                        "//chess-board/div[contains(@class, 'highlight')][1]")
+        start = self.browser.find_element(By.XPATH,
+                                          "//chess-board/div[contains(@class, 'highlight')][2]")
         hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
         while hod == old_hod or hod[2:] + hod[:2] == old_hod:
-            end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[2]")
-            start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[3]")
+            end = self.browser.find_element(By.XPATH, "//chess-board/div[2]")
+            start = self.browser.find_element(By.XPATH, "//chess-board/div[3]")
             hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
             if old_hod != hod and hod[2:] + hod[:2] != old_hod:
-                sleep(0.5)
-                end = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[2]")
-                start = self.browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/chess-board/div[3]")
+                sleep(0.3)
+                end = self.browser.find_element(By.XPATH, "//chess-board/div[2]")
+                start = self.browser.find_element(By.XPATH, "//chess-board/div[3]")
                 hod = to_bot(start.get_attribute("class")) + to_bot(end.get_attribute("class"))
         return hod
 
 
-    def move(self, element, x, y, is_end, figura):
+    def move(self, element, x, y, is_end, figura, mode):
         piece = self.browser.find_element(By.CSS_SELECTOR, element)
         action = webdriver.common.action_chains.ActionChains(self.browser)
         action.move_to_element_with_offset(piece, 5, 5)
         action.click()
         action.perform()
-        print("click")
-        print(element, x, y)
-        sleep(0.5)
+        sleep(0.1)
         hint = self.browser.find_element(By.CSS_SELECTOR, f".square-{x}{y}")
         action = webdriver.common.action_chains.ActionChains(self.browser)
         action.move_to_element_with_offset(hint, 5, 5)
         action.click()
         action.perform()
         if is_end:
-            self.browser.find_element(By.CSS_SELECTOR, f'[class="promotion-piece w{figura}"]').click()
+            self.browser.find_element(By.CSS_SELECTOR, f'[class="promotion-piece {mode}{figura}"]').click()
 
 
 
@@ -76,17 +76,20 @@ def to_bot(hod):
     return hod
 
 def main():
+    hod = ""
     board = chess.Board()
     board_player = Board()
     engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-    limit = chess.engine.Limit(time=1.0)
+    limit = chess.engine.Limit(time=0.5)
 
     input("Зайдите в игру и нажмите Enter")
-    # mode = input("Введите w  или  b")
-    c = 0
+    mode = input("Введите w  или  b: ")
+    if mode == "w":
+        c = 0
+    else:
+        c = 1
     while True:
         if c == 1:
-            print("black")
             hod = board_player.parse(hod)
             move = chess.Move.from_uci(hod)
             if move in board.legal_moves:
@@ -98,12 +101,11 @@ def main():
                 move = chess.Move.from_uci(hod)
                 board.push(move)
         else:
-            print("white")
             hod = str(engine.play(board, limit).move)
             move = chess.Move.from_uci(hod)
             board.push(move)
             print(hod)
-            board_player.move(*to_chess_com(hod))
+            board_player.move(*to_chess_com(hod), mode)
             if len(hod) > 4:
                 hod = hod[:4]
         c += 1
